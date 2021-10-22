fbab32196a850a6b840ee600383a055a0af7e0da
cxx3668119 <862704268@qq.com>
2021-10-22 14:14:27 +0800

    主要功能

---
+++ search-component/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <demo />
+  </div>
+</template>
+
+<script>
+  import Demo from './components/Demo.vue'
+
+  export default {
+    name: "App",
+    components: { Demo },
+  };
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ search-component/src/components/Demo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="serach-component">
+    <div class="input-box">
+      <input type="number" placeholder="请输入分数" @input="setScore" :value="searchedScore" name="" id="">
+      <button @click="setFilterReg('up')" :class="['regular-btn',{active: filterReg === 'up'}]">以上</button>
+      <button @click="setFilterReg('down')" :class="['regular-btn',{active: filterReg === 'down'}]">以下</button>
+    </div>
+    <div class="menu" v-show="menuShow">
+      <ul>
+        <li v-for="item in menuList" :key="item.id">
+          <input type="checkbox">
+          <span>{{item.name}}</span>
+          <span>{{item.score}}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import students from '../data/students.js'
+  export default {
+    name: 'Demo',
+    data() {
+      return {
+        searchedScore: 0,
+        filterReg: 'up',
+        menuShow: "false",
+        menuList: []
+      }
+    },
+    methods: {
+      setScore(e) {
+        this.searchedScore = Number(e.target.value)
+        this.filterStudents()
+      },
+      setFilterReg(reg) {
+        this.filterReg = reg
+        this.filterStudents()
+      },
+      filterStudents() {
+        console.log(students);
+        switch (this.filterReg) {
+          case 'up':
+            this.menuList = students.filter(item =>
+              item.score >= this.searchedScore
+            )
+            break
+          case 'down':
+            this.menuList = students.filter(item =>
+              item.score < this.searchedScore
+            )
+            break
+          default:
+            break
+        }
+        this.menuShow = this.menuList.length > 0
+        console.log(this.menuList);
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .active {
+    background-color: #000;
+    color: #fff;
+  }
+</style>
